bc4a87b4f5d426c4bbf7f0b5776d675c055ea93b
aalmiray <aalmiray@gmail.com>
2012-03-09 23:17:04 +0100

    Updated to latest Griffon  release

--- demos/numberguess/griffon-app/conf/Builder.groovy
+++ demos/numberguess/griffon-app/conf/Builder.groovy
@@ -1,16 +1,11 @@
+
+root.'I18nGriffonAddon'.addon=true
+
+root.'ActionsGriffonAddon'.addon=true
+
 root {
     'groovy.swing.SwingBuilder' {
         controller = ['Threading']
         view = '*'
     }
-    'griffon.app.ApplicationBuilder' {
-        view = '*'
-    }
 }
-root.'WeldGriffonAddon'.addon=true
-
-root.'MiglayoutGriffonAddon'.addon=true
-
-root.'I18nGriffonAddon'.addon=true
-
-root.'ActionsGriffonAddon'.addon=true
