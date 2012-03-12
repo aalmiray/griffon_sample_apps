59a8343494e423209d53058e1ddfcc8efdea2d1a
aalmiray <aalmiray@gmail.com>
2012-03-12 13:41:50 +0100

    Upgrade to Griffon 0.9.5-rc2

--- 3d/jogl/griffon-app/conf/Builder.groovy
+++ 3d/jogl/griffon-app/conf/Builder.groovy
@@ -1,12 +1,7 @@
+
 root {
     'groovy.swing.SwingBuilder' {
         controller = ['Threading']
         view = '*'
     }
-    'griffon.app.ApplicationBuilder' {
-        view = '*'
-    }
 }
-
-
-root.'JoglGriffonAddon'.addon=true
