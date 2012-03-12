d56b482767766db87a0eed0f7d5d73964248d9be
aalmiray <aalmiray@gmail.com>
2012-03-12 14:22:40 +0100

    Updated to Griffon 0.9.5

--- 3d/lwjgl/griffon-app/conf/Builder.groovy
+++ 3d/lwjgl/griffon-app/conf/Builder.groovy
@@ -1,14 +1,9 @@
+
 root {
     'groovy.swing.SwingBuilder' {
         controller = ['Threading']
         view = '*'
     }
-    'griffon.app.ApplicationBuilder' {
-        view = '*'
-    }
 }
 
 root.'griffon.builder.trident.TridentBuilder'.view = '*'
-
-
-root.'LwjglGriffonAddon'.addon=true
